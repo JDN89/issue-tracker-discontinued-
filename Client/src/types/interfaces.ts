6ad7ac4ce8d5b8cbac8c0074aa29e64c09f058c2
Jan De Niels <jan.de.niels@gmail.comm>
2022-02-24 16:35:04 -0500

    make color palette black white gray

--- Client/src/types/interfaces.ts
+++ Client/src/types/interfaces.ts
@@ -1,7 +1,3 @@
-export interface CreateUserInterface {
-  email: string
-  password: string
-}
 
 export interface LoginUserInterface {
   email: string
@@ -14,39 +10,7 @@ export interface RegisterUserInterface {
   password: string
 }
 
-export interface UserInterface {
-  id: number
-  email: string
-  password: string
-}
 export interface NewMessageInterface {
   Title: string
   Body: string
-}
-
-export interface ToDoListInterface {
-  listId: number
-  // primary key
-  userId: number | null
-  // foreign key
-  title: string
-  todos: string[]
-
-  archived: string[]
-}
-export interface newListInterface {
-  // userId: number | null;
-  title: string
-  todos: string[]
-}
-export interface TodoInterface {
-  // userId: number | null;
-  listId: number
-  todo: string
-}
-
-export interface MessageInterface {
-  id: number
-  title: string
-  body: string
 }
