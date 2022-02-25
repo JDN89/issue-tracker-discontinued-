658b829451fbc2b4df6f0bcb7b0caa4ed03fb0e1
Jan De Niels <jan.de.niels@gmail.comm>
2022-02-25 11:36:37 -0500

    reformat headerGuest

--- Client/src/components/HeaderGuest.vue
+++ Client/src/components/HeaderGuest.vue
@@ -19,19 +19,17 @@ import { isDark, toggleDark } from '../composables'
         title="Register"
       >
         <div class="i-carbon-group-account" />
-
       </router-link>
-      <div class="icon-btn mx-2"></div>
-      <div class="icon-btn mx-2"></div>
- <a
-      class="icon-btn mx-2"
-      i-carbon-logo-github
-      rel="noreferrer"
-      href="https://github.com/JDN89/issue-tracker"
-      target="_blank"
-      title="GitHub"
-    />
-
+      <div class="icon-btn mx-2" />
+      <div class="icon-btn mx-2" />
+      <a
+        class="icon-btn mx-2"
+        i-carbon-logo-github
+        rel="noreferrer"
+        href="https://github.com/JDN89/issue-tracker"
+        target="_blank"
+        title="GitHub"
+      />
 
       <button class="icon-btn mx-2 !outline-none" @click="toggleDark()">
         <div v-if="isDark" i-carbon-moon />
