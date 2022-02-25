d6d7b6889499205eb2384528d9dcd1cc060afdec
Jan De Niels <jan.de.niels@gmail.comm>
2022-02-25 11:35:51 -0500

    check token en username present in localStorage before loaidng app.vue

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+
+import { useRouter } from 'vue-router'
+
+onBeforeMount(async() => {
+  const router = useRouter()
+  const token = window.localStorage.getItem('token')
+  const username = window.localStorage.getItem('username')
+
+  if (token && username)
+
+    await router.push(`/hi/${encodeURIComponent(username)}`)
+})
+
+</script>
+
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
     <router-view />
