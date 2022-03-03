ca77a58b228f9b558e7e76bf3ab9c53225ef4910
Jan De Niels <jan.de.niels@gmail.comm>
2022-03-03 09:59:39 -0500

    dipsplay username in upper case

--- Client/src/pages/hi/[username].vue
+++ Client/src/pages/hi/[username].vue
@@ -11,7 +11,7 @@ const props = defineProps<{ username: string }>()
   <div id="container" class="flex flex-row mx-auto justify-center">
     <div id="main" class="order-2 flex-1 h-96 bg-blue-100">
       <h1 text-2xl>
-        Hi, {{ props.username }}
+        Hi, {{ props.username.toUpperCase() }}
       </h1>
       <KabanBoard />
     </div>
