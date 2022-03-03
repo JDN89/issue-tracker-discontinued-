df203760ad7aa94b5c75fb34d10b0035e31503ab
Jan De Niels <jan.de.niels@gmail.comm>
2022-03-03 09:45:56 -0500

    Placed sidebar inside a component

---
+++ Client/src/components/SideBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useProjectStore } from '~/stores/projects'
+
+const store = useProjectStore()
+</script>
+
+<template>
+  <div id="sidebar" class="min-w-24 max-w-44 bg-red-400 order-1 flex-1">
+    <p>test</p>
+    <ul>
+      <li v-for="project in store.getProjects" :key="project.id" class="square-border">
+        {{ project.title }}
+      </li>
+    </ul>
+    <button btn>
+      add project
+    </button>
+  </div>
+</template>
