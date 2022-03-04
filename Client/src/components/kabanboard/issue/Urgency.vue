083030d9f41ec658a20624782e4782425c0a0366
Jan De Niels <jan.de.niels@gmail.comm>
2022-03-04 15:31:26 -0500

    create urgency badge color

---
+++ Client/src/components/kabanboard/issue/Urgency.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{ urgency: string } >()
+
+const myUrgencyColorMap = new Map<string, string>([['low', 'text-yellow-800 bg-yellow-100'], ['medium', 'text-orange-800 bg-orange-200'], ['high', 'text-red-800 bg-red-200']])
+
+const urgencyColor = computed(() => {
+  return myUrgencyColorMap.get(props.urgency)!
+})
+
+</script>
+<template>
+  <div
+    class=" px-3 h-6 rounded-full text-xs font-semibold flex items-center "
+    :class="`${urgencyColor}` "
+  >
+    <span class="w-2 h-2 rounded-full mr-1" />
+    <span>
+      <slot />
+    </span>
+  </div>
+</template>
