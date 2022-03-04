608a5fe97590a062f38386602ece7aca8ee115d8
Jan De Niels <jan.de.niels@gmail.comm>
2022-03-04 14:45:44 -0500

    refactor KabanBoard and place code inside components

--- Client/src/components/KabanBoard.vue
+++ Client/src/components/KabanBoard.vue
@@ -1,37 +1,22 @@
 <script setup lang="ts">
-import draggable from 'vuedraggable'
 
-import { useProjectStore } from '~/stores/projects'
-
-const store = useProjectStore()
-
-watch(store.getOpenIssues!, (value) => {
-  store.updateOpenIssuesDb(value)
-})
+import IssuesInProgress from './IssuesInProgress.vue'
 
 </script>
 
 <template>
-  <div id="Kaban-Board" class="flex flex-1 rounded-border flex-grow-1 flex-shrink-0 flex-row">
+  <div id="Kaban-Board" class="flex justify-between flex-1 rounded-border flex-grow-1 flex-shrink-0 flex-row">
     <div class="flex-1 min-w-34 max-w-60 order-1 min-h-lg max-h-screen ">
       <h1 text-3xl>
         openIssue
       </h1>
-
-      <draggable
-
-        :list="store.getOpenIssues!"
-        group="Issues"
-        item-key="id"
-        ghost-class="ghost"
-        animation=" 100"
-      >
-        <template #item="{ element }">
-          <div class=" square-border border-shadow m-2 ">
-            <IssueCard :title="element.title" :urgency="element.urgency" :date="element.date" :type="element.type" />
-          </div>
-        </template>
-      </draggable>
+      <OpenIssues />
+    </div>
+    <div class="flex-1 min-w-34 max-w-60 order-1 min-h-lg max-h-screen ">
+      <h1 text-3xl>
+        In Progress
+      </h1>
+      <IssuesInProgress />
     </div>
   </div>
 </template>
