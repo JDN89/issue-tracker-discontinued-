485fd779ef07e565911d9ad19df06db1fc84ff34
Jan De Niels <jan.de.niels@gmail.comm>
2022-03-04 14:46:14 -0500

    Create IssueInprogress component

---
+++ Client/src/components/IssuesInProgress.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import draggable from 'vuedraggable'
+
+import { useProjectStore } from '~/stores/projects'
+
+const store = useProjectStore()
+
+watch(store.getIssuesInProgress!, async(value) => {
+  await store.updateIssueInProgressDb(value)
+})
+
+</script>
+
+<template>
+  <draggable
+
+    :list="store.getIssuesInProgress!"
+    group="Issues"
+    item-key="id"
+    ghost-class="ghost"
+    animation=" 100"
+  >
+    <template #item="{ element }">
+      <div class=" square-border border-shadow m-2 ">
+        <IssueCard :title="element.title" :urgency="element.urgency" :date="element.date" :type="element.type" />
+      </div>
+    </template>
+  </draggable>
+</template>
