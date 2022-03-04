584f3517f9f4d9b30ea5b99566d60c8938c82a32
Jan De Niels <jan.de.niels@gmail.comm>
2022-03-04 10:07:50 -0500

    delete unused style prop in sidebard.vue

--- Client/src/components/SideBar.vue
+++ Client/src/components/SideBar.vue
@@ -5,7 +5,7 @@ const store = useProjectStore()
 </script>
 
 <template>
-  <div id="sidebar" class="min-w-24 max-w-44 order-1 flex-1">
+  <div id="sidebar" class="min-w-24 max-w-44 flex-1">
     <h1 text-3xl py-3>
       My Projects
     </h1>
