01883deb5ca49cd81b5bb929a4e59aea0fb93f9f
Jan De Niels <jan.de.niels@gmail.comm>
2022-03-05 19:04:21 -0500

    Add RevieuwIssues and ClosedIssues to the KabanBoard

--- Client/src/components/kabanboard/KabanBoard.vue
+++ Client/src/components/kabanboard/KabanBoard.vue
@@ -3,18 +3,30 @@
 </script>
 
 <template>
-  <div id="Kaban-Board" class="flex justify-between flex-1 rounded-border flex-grow-1 flex-shrink-0 flex-row">
-    <div class="flex-1 min-w-34 max-w-60 order-1 min-h-lg max-h-screen ">
-      <h1 text-3xl>
+  <div id="Kaban-Board" class="flex gap-3 flex-row justify-around flex-1 ">
+    <div class="min-w-34 max-w-80 ">
+      <h1 text-3xl mb-5>
         openIssue
       </h1>
       <OpenIssues />
     </div>
-    <div class="flex-1 min-w-34 max-w-60 order-1 min-h-lg max-h-screen ">
-      <h1 text-3xl>
+    <div class="min-w-24 max-w-80 ">
+      <h1 text-3xl mb-5>
         In Progress
       </h1>
       <IssuesInProgress />
+    </div>
+    <div class="min-w-34 max-w-80 ">
+      <h1 text-3xl mb-5>
+        Review
+      </h1>
+      <ReviewIssues />
+    </div>
+    <div class="min-w-34 max-w-80 ">
+      <h1 text-3xl mb-5>
+        Closed
+      </h1>
+      <ClosedIssues />
     </div>
   </div>
 </template>
