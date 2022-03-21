64a6c921643c098a333fa8a6931d7ea38a4bce60
Jan De Niels <jan.de.niels@gmail.comm>
2022-03-21 12:43:45 -0500

    if token in sessions => store token in userStore

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import { useUserStore } from './stores/users'
 import { useProjectStore } from '~/stores/projects'
 
 onBeforeMount(async() => {
   const router = useRouter()
+  const userStore = useUserStore()
   const token = window.localStorage.getItem('token')
-  const username = window.localStorage.getItem('username')
   const store = useProjectStore()
+  if (token)
+    userStore.token = token
+  const username = window.localStorage.getItem('username')
 
   if (token && username)
-
     await router.push(`/hi/${encodeURIComponent(username)}`)
     // fetch project[0].id
   await store.fetchProjects()
