06a225a00501de67972a7b01892766607a296a63
Jan De Niels <jan.de.niels@gmail.comm>
2022-03-23 21:04:51 -0500

    Clean up code in project.ts

--- Client/src/stores/projects.ts
+++ Client/src/stores/projects.ts
@@ -31,13 +31,6 @@ export const useProjectStore = defineStore({
   }),
 
   actions: {
-    // TOODO: delete once db is connected
-    generateGuid() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        const r = Math.random() * 16 | 0; const v = c === 'x' ? r : (r & 0x3 | 0x8)
-        return v.toString(16)
-      })
-    },
 
     // =========================================
     // ===========   FETCH Projects  ===============
