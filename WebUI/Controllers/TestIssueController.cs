1746ff45481fa5fb1dad2ea3a98544750094cd77
JDN89 <jan.de.niels@gmail.comm>
2022-03-24 09:00:43 -0500

    See if github is now tracking your changes

--- WebUI/Controllers/TestIssueController.cs
+++ WebUI/Controllers/TestIssueController.cs
@@ -15,4 +15,15 @@ public class TestIssueController:BaseController
         {
             return await Mediator.Send(new GetAllTestIssues.Query{ProjId = projectId});
         }
+        
+    [HttpPut("{projectId}")]
+    [Authorize]
+    public async Task<IResult> UpdateAllOpenIssues([FromBody] List<GetOpenIssueDto> openIssues, Guid projectId)
+    {
+        await Mediator.Send(new DeleteAllOpenIssuesInProject.Command {ProjectId = projectId});
+        if (openIssues.Count <= 0) return Results.Ok();
+        await Mediator.Send(new AddOpenIssues.Command {Issues = openIssues});
+
+        return Results.Ok();
+    }
 }
